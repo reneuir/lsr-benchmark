b4e7064639ec4d6b39f60dbd8db131286cd2fe57
Maik Fröbe <maik.froebe@uni-jena.de>
2026-04-07 16:34:18 +0200

    mf

--- step-02-embedding-approaches/hypencoder/hypencoder-reranker.py
+++ step-02-embedding-approaches/hypencoder/hypencoder-reranker.py
@@ -4,6 +4,7 @@ import click
 import ir_datasets
 from tirex_tracker import ExportFormat, register_metadata, tracking
 
+from tqdm import tqdm
 from hypencoder_cb.modeling.hypencoder import Hypencoder, HypencoderDualEncoder, TextEncoder
 from transformers import AutoTokenizer
 
@@ -22,22 +23,40 @@ def main(dataset: str, model: str, output: Path):
     dual_encoder = HypencoderDualEncoder.from_pretrained(model)
     tokenizer = AutoTokenizer.from_pretrained(model)
 
+    import torch
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Use device", device)
     query_encoder: Hypencoder = dual_encoder.query_encoder
+    query_encoder.to(device)
     passage_encoder: TextEncoder = dual_encoder.passage_encoder
+    passage_encoder.to(device)
 
-    queries = [{"docno": i.query_id, "text": i.default_text()} for i in ir_dataset.queries_iter()]
+    queries = [{"qid": i.query_id, "text": i.default_text()} for i in ir_dataset.queries_iter()]
     documents = [{"docno": i.doc_id, "text": i.default_text()} for i in ir_dataset.docs_iter()]
 
     query_inputs = tokenizer([i["text"] for i in queries], return_tensors="pt", padding=True, truncation=True)
-    document_inputs = tokenizer([i["text"] for i in documents], return_tensors="pt", padding=True, truncation=True)
+    query_inputs.to(device)
+
+    #document_inputs = tokenizer([i["text"] for i in documents], return_tensors="pt", padding=True, truncation=True)
+    #document_inputs.to(device)
+    qnets = {}
 
     with tracking(export_file_path=output / f"embedding-ir-metadata.yml"):
-        q_nets = query_encoder(input_ids=query_inputs["input_ids"], attention_mask=query_inputs["attention_mask"]).representation
-        document_embeddings = passage_encoder(input_ids=document_inputs["input_ids"], attention_mask=document_inputs["attention_mask"]).representation
+        for idx in tqdm(range(len(queries)), "encode queries"):
+            input_ids = torch.tensor([query_inputs["input_ids"][0][idx]])
+            input_ids.to(device)
+            attention_mask = torch.tensor([query_inputs["attention_mask"][0][idx]])
+            attention_mask.to(device)
+
+            q_net = query_encoder(input_ids=input_ids, attention_mask=attention_mask).representation
+            qnets[queries[idx]["qid"]] = q_net.state_dict()
+
+        #document_embeddings = passage_encoder(input_ids=document_inputs["input_ids"], attention_mask=document_inputs["attention_mask"]).representation
 
 
     #document_embeddings_single = document_embeddings.unsqueeze(1)
     print(dir(q_nets))
+    print(type(q_nets))
 
 if __name__ == '__main__':
     main()
